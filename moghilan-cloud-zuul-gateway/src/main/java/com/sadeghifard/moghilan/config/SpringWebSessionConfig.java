ac19de6175517d9e73b2db1817f84955687850ca
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-21 07:08:47 -0800

    Develop

---
+++ moghilan-cloud-zuul-gateway/src/main/java/com/sadeghifard/moghilan/config/SpringWebSessionConfig.java
@@ -0,0 +1,27 @@
+package com.sadeghifard.moghilan.config;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.ReactiveMapSessionRepository;
+import org.springframework.session.ReactiveSessionRepository;
+import org.springframework.session.config.annotation.web.server.EnableSpringWebSession;
+import org.springframework.web.server.session.HeaderWebSessionIdResolver;
+import org.springframework.web.server.session.WebSessionIdResolver;
+
+@EnableSpringWebSession
+public class SpringWebSessionConfig {
+	
+    @Bean
+    public ReactiveSessionRepository<?> sessionRepository() {
+        return new ReactiveMapSessionRepository(new ConcurrentHashMap<>());
+    }
+
+    @Bean
+    public WebSessionIdResolver headerWebSessionIdResolver() {
+        HeaderWebSessionIdResolver resolver = new HeaderWebSessionIdResolver();
+        resolver.setHeaderName("X-SESSION-ID");
+        return resolver;
+    }
+
+}
---
+++ moghilan-cloud-zuul-gateway/src/main/java/com/sadeghifard/moghilan/filters/PreFilter.java
@@ -0,0 +1,32 @@
+package com.sadeghifard.moghilan.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class PreFilter extends ZuulFilter {
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
