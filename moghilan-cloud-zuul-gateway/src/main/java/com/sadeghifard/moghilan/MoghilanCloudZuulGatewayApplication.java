2760731868ce5c11a355eeba8fddefcd9b1cb8cb
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-22 01:29:49 -0800

    Develp

---
+++ moghilan-cloud-zuul-gateway/src/main/java/com/sadeghifard/moghilan/MoghilanCloudZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.sadeghifard.moghilan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class MoghilanCloudZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoghilanCloudZuulGatewayApplication.class, args);
+	}
+
+}
