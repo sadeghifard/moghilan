113b78f623c132c7d5c7892f44376e0d37c8fb45
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-22 03:32:20 -0800

    Develop

---
+++ moghilan-cloud-zuul-gateway/src/main/java/com/sadeghifard/moghilan/config/security/UserDetailsService.java
@@ -0,0 +1,7 @@
+package com.sadeghifard.moghilan.config.security;
+
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+
+public class UserDetailsService extends MapReactiveUserDetailsService {
+
+}
