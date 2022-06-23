8bc9fc990c90a05d3146a1c632ada92bbd8fe80f
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-23 11:18:23 -0800

    Develop

--- moghilan-cloud-customer-microservice/src/main/java/com/sadeghifard/moghilan/service/impl/CustomerService.java
+++ moghilan-cloud-customer-microservice/src/main/java/com/sadeghifard/moghilan/service/impl/CustomerService.java
@@ -1,18 +1,19 @@
 package com.sadeghifard.moghilan.service.impl;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
-import com.sadeghifard.moghilan.exception.ResourceException;
+import com.sadeghifard.moghilan.exception.ResourceAlreadyReportedException;
+import com.sadeghifard.moghilan.exception.ResourceNotAcceptableException;
+import com.sadeghifard.moghilan.exception.ResourceNotFoundException;
 import com.sadeghifard.moghilan.model.Customer;
 import com.sadeghifard.moghilan.repository.CustomerRepository;
 import com.sadeghifard.moghilan.service.ICustomerService;
-
 import lombok.AllArgsConstructor;
+
 @Service
 @Transactional
 @AllArgsConstructor
@@ -25,26 +26,26 @@ public class CustomerService implements ICustomerService{
 		try {
 			return customerRepository.findAll();
 		} catch (Exception e) {
-			throw new ResourceException("Customer", "Get All", null);
+			throw new ResourceNotFoundException("Customer", "Get All", null);
 		}
 	}
 
 	@Override
 	public Customer getCustomerById(Long id) {
 		return customerRepository.findById(id)
-				.orElseThrow(() -> new ResourceException("Customer", "Customer ID", id));
+				.orElseThrow(() -> new ResourceNotFoundException("Customer", "Customer ID", id));
 	}
 
 	@Override
 	public Customer getByCustomerNumber(Long customerNumber) {
 		return customerRepository.findByCustomerNumber(customerNumber)
-				.orElseThrow(() -> new ResourceException("Customer", "Customer Number", customerNumber));
+				.orElseThrow(() -> new ResourceNotFoundException("Customer", "Customer Number", customerNumber));
 	}
 
 	@Override
 	public Customer getByNationalCode(Long nationalCode) {
 		return customerRepository.findByNationalCode(nationalCode)
-				.orElseThrow(() -> new ResourceException("Customer", "National Code", nationalCode));
+				.orElseThrow(() -> new ResourceNotFoundException("Customer", "National Code", nationalCode));
 	}
 
 	@Override
@@ -53,7 +54,7 @@ public class CustomerService implements ICustomerService{
 			customer.setCreateDate(LocalDateTime.now());
 			return customerRepository.save(customer);
 		} catch (Exception e) {
-			throw new ResourceException("Customer", "Save Customer", customer);
+			throw new ResourceAlreadyReportedException("Customer", "Save Customer", customer);
 		}
 	}
 	
@@ -63,62 +64,90 @@ public class CustomerService implements ICustomerService{
 			customer.setModifyDate(LocalDateTime.now());
 			return customerRepository.save(customer);
 		} catch (Exception e) {
-			throw new ResourceException("Customer", "update Customer", customer);
+			throw new ResourceNotAcceptableException("Customer", "Update Customer", customer);
 		}
 	}
 
 	@Override
 	public Customer updateById(Customer customer, Long id) {
-		Customer existCustomer = getCustomerById(id);
-		if(existCustomer != null) {
-			customer.setModifyDate(LocalDateTime.now());
-			existCustomer = customerRepository.save(customer);
+		try {
+			Customer existCustomer = getCustomerById(id);
+			if(existCustomer != null) {
+				customer.setModifyDate(LocalDateTime.now());
+				existCustomer = customerRepository.save(customer);
+			}
+			return existCustomer;
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Customer", "Customer ID", customer);
 		}
-		return existCustomer;
 	}
 	
 	@Override
 	public Customer updateByCustomerNumber(Customer customer, Long customerNumber) {
-		Customer existCustomer = getByCustomerNumber(customerNumber);
-		if(existCustomer != null) {
-			customer.setModifyDate(LocalDateTime.now());
-			existCustomer = customerRepository.save(customer);
+		try {
+			Customer existCustomer = getByCustomerNumber(customerNumber);
+			if(existCustomer != null) {
+				customer.setModifyDate(LocalDateTime.now());
+				existCustomer = customerRepository.save(customer);
+			}
+			return existCustomer;
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Customer", "Customer Number", customer);
 		}
-		return existCustomer;
 	}
 
 
 	@Override
 	public Customer updateByNationalCode(Customer customer, Long nationalCode) {
-		Customer existCustomer = getByNationalCode(nationalCode);
-		if(existCustomer != null) {
-			customer.setModifyDate(LocalDateTime.now());
-			existCustomer = customerRepository.save(customer);
-		}
+		try {
+			Customer existCustomer = getByNationalCode(nationalCode);
+			if(existCustomer != null) {
+				customer.setModifyDate(LocalDateTime.now());
+				existCustomer = customerRepository.save(customer);
+			}
 		return existCustomer;
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Customer", "Customer Natinal Code", nationalCode);
+		}
 	}
 	
 	@Override
 	public String deleteCustomer(Customer customer) {
-		customerRepository.delete(customer);
-		return "Customer deleted successfully";
+		try {
+			customerRepository.delete(customer);
+			return "Customer deleted successfully";
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Customer", "Delete Customer", customer);
+		}
 	}
 
 	@Override
 	public String deleteByCustomerNumber(Long customerNumber) {
-		customerRepository.deleteByCustomerNumber(customerNumber);
-		return "Customer deleted successfully by account number = " + customerNumber;
+		try {
+			customerRepository.deleteByCustomerNumber(customerNumber);
+			return "Customer deleted successfully by account number = " + customerNumber;
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Customer", "Customer Number", customerNumber);
+		}
 	}
 
 	@Override
 	public String deleteById(Long id) {
-		customerRepository.deleteById(id);
-		return "Customer deleted successfully by ID = " + id;
+		try {
+			customerRepository.deleteById(id);
+			return "Customer deleted successfully by ID = " + id;
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Customer", "Customer ID", id);
+		}
 	}
 
 	@Override
 	public String deleteByNationalCode(Long nationalCode) {
-		customerRepository.deleteByNationalCode(nationalCode);
-		return "Customer deleted successfully by national code = " + nationalCode;
+		try {
+			customerRepository.deleteByNationalCode(nationalCode);
+			return "Customer deleted successfully by national code = " + nationalCode;
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Customer", "Customer National Code", nationalCode);
+		}
 	}
 }
