ed416e6aa4a8e0727dca260750fc2b4a74e270e3
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-23 07:32:37 -0800

    Develop

--- moghilan-cloud-account-microservice/src/main/java/com/sadeghifard/moghilan/service/IAccountService.java
+++ moghilan-cloud-account-microservice/src/main/java/com/sadeghifard/moghilan/service/IAccountService.java
@@ -1,16 +1,19 @@
 package com.sadeghifard.moghilan.service;
 
+import com.sadeghifard.moghilan.exception.ResourceAlreadyReportedException;
+import com.sadeghifard.moghilan.exception.ResourceNotAcceptableException;
+import com.sadeghifard.moghilan.exception.ResourceNotFoundException;
 import com.sadeghifard.moghilan.model.Account;
 
 public interface IAccountService {
-	Iterable<Account> getAllAccounts();
-	Account getAccountById(Long id);
-	Account getAccountByNumber(Long accountNumber);
-	Iterable<Account> getAccountsByCustomerNationalCode(Long customerNationalCode);
-	Account saveAccount(Account account);
-	Account updateAccount(Account account);
-	Account updateByAccountNumber(Account account, Long accountNumber);
-	Account updateById(Account account, Long id);
+	Iterable<Account> getAllAccounts() throws ResourceNotFoundException;
+	Account getAccountById(Long id) throws ResourceNotFoundException;
+	Account getAccountByNumber(Long accountNumber) throws ResourceNotFoundException;
+	Iterable<Account> getAccountsByCustomerNationalCode(Long customerNationalCode) throws ResourceNotFoundException;
+	Account saveAccount(Account account) throws ResourceAlreadyReportedException;
+	Account updateAccount(Account account) throws ResourceNotAcceptableException;
+	Account updateByAccountNumber(Account account, Long accountNumber) throws ResourceNotFoundException;
+	Account updateById(Account account, Long id) throws ResourceNotFoundException;
 	String deleteAccount(Account account);
 	String deleteByAccountNumber(Long accountNumber);
 	String deleteById(Long id);
--- moghilan-cloud-account-microservice/src/main/java/com/sadeghifard/moghilan/service/ICartService.java
+++ moghilan-cloud-account-microservice/src/main/java/com/sadeghifard/moghilan/service/ICartService.java
@@ -1,17 +1,20 @@
 package com.sadeghifard.moghilan.service;
 
 
+import com.sadeghifard.moghilan.exception.ResourceAlreadyReportedException;
+import com.sadeghifard.moghilan.exception.ResourceNotAcceptableException;
+import com.sadeghifard.moghilan.exception.ResourceNotFoundException;
 import com.sadeghifard.moghilan.model.Cart;
 
 
 public interface ICartService {
-	Iterable<Cart> getAllCarts();
-	Cart getCartById(Long id);
-	Cart getCartByCartNumber(Long cartNumber);
-	Cart saveCart(Cart cart);
-	Cart updateCart(Cart cart);
-	Cart updateCartById(Cart cart, Long id);
-	Cart updateCartByCartNumber(Cart cart, Long cartNumber);
+	Iterable<Cart> getAllCarts() throws ResourceNotFoundException;
+	Cart getCartById(Long id) throws ResourceNotFoundException;
+	Cart getCartByCartNumber(Long cartNumber) throws ResourceNotFoundException;
+	Cart saveCart(Cart cart) throws ResourceAlreadyReportedException;
+	Cart updateCart(Cart cart) throws ResourceNotAcceptableException;
+	Cart updateCartById(Cart cart, Long id) throws ResourceNotFoundException;
+	Cart updateCartByCartNumber(Cart cart, Long cartNumber) throws ResourceNotFoundException;
 	String deleteCart(Cart cart);
 	String deleteByCartNumber(Long cartNumber);
 	String deleteById(Long id);
