df6d40ae84b449e7ff018b677f02d618f295c071
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-23 06:57:54 -0800

    Develop

---
+++ moghilan-cloud-feign-client/src/main/java/com/sadeghifard/moghilan/exception/ResourceNotAcceptableException.java
@@ -0,0 +1,20 @@
+package com.sadeghifard.moghilan.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
+public class ResourceNotAcceptableException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	private String resourceName;
+	private String fieldname;
+	private Object fieldValue;
+	
+	public ResourceNotAcceptableException(String resourceName, String fieldName, Object fieldValu) {
+		super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldName));
+		this.resourceName = resourceName;
+		this.fieldname = fieldName;
+		this.fieldValue = fieldValue;
+	}
+}
