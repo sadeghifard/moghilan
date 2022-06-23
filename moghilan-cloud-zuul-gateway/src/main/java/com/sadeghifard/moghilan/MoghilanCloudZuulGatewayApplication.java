bcb692292891c4e8dcc9f15bfdd49aa6730c5ed6
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-23 04:11:27 -0800

    Develop

--- moghilan-cloud-zuul-gateway/src/main/java/com/sadeghifard/moghilan/MoghilanCloudZuulGatewayApplication.java
+++ moghilan-cloud-zuul-gateway/src/main/java/com/sadeghifard/moghilan/MoghilanCloudZuulGatewayApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.sadeghifard.moghilan.filters.ErrorFilter;
+import com.sadeghifard.moghilan.filters.PostFilter;
+import com.sadeghifard.moghilan.filters.PreFilter;
+import com.sadeghifard.moghilan.filters.RouteFilter;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +18,26 @@ public class MoghilanCloudZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoghilanCloudZuulGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 
 }
