ef74476a9e258ffb99946be6b406ecfc8aa5d26d
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-23 20:45:15 -0800

    Develop

--- moghilan-cloud-customer-microservice/src/main/java/com/sadeghifard/moghilan/enums/Gender.java
+++ moghilan-cloud-customer-microservice/src/main/java/com/sadeghifard/moghilan/enums/Gender.java
@@ -1,6 +1,19 @@
 package com.sadeghifard.moghilan.enums;
 
-public enum Gender {
-	Male, Female
+import com.fasterxml.jackson.annotation.JsonValue;
 
+public enum Gender {
+	MALE("MALE"), 
+	FEMALE("FEMALE");
+	
+	private String type;
+	
+	Gender(String type) {
+		this.type = type;
+	}
+	
+	@JsonValue
+	public String getType() {
+		return type;
+	}
 }
--- moghilan-cloud-payment-microservice/src/main/java/com/sadeghifard/moghilan/enums/PaymentType.java
+++ moghilan-cloud-payment-microservice/src/main/java/com/sadeghifard/moghilan/enums/PaymentType.java
@@ -1,5 +1,20 @@
 package com.sadeghifard.moghilan.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PaymentType {
-	WITHDRAW, DEPOSIT, BORROW
+	WITHDRAW("WITHDRAW"), 
+	DEPOSIT("DEPOSIT"), 
+	BORROW("BORROW");
+	
+	private String type;
+	
+	PaymentType(String type) {
+		this.type = type;
+	}
+	
+	@JsonValue
+	public String getType() {
+		return type;
+	}
 }
