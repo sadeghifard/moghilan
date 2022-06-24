376c6a8537fcce6d911d34ce938a5aa066db0a46
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-23 21:05:45 -0800

    Develop

--- moghilan-cloud-feign-client/src/main/java/com/sadeghifard/moghilan/service/WebAccountService.java
+++ moghilan-cloud-feign-client/src/main/java/com/sadeghifard/moghilan/service/WebAccountService.java
@@ -10,19 +10,43 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.sadeghifard.moghilan.domain.Account;
+import com.sadeghifard.moghilan.exception.ResourceAlreadyReportedException;
+import com.sadeghifard.moghilan.exception.ResourceNotAcceptableException;
+import com.sadeghifard.moghilan.exception.ResourceNotFoundException;
 
 @FeignClient("account-microservice")
 public interface WebAccountService {
 	
-	@GetMapping("/account/nc/{cusNationalCod}")
-	ResponseEntity<Iterable<Account>> getAccountsByCustomerNationalCode (@PathVariable Long cusNationalCod);
+	@GetMapping("/account")
+	ResponseEntity<Iterable<Account>> getAllAccounts();
 	
-	@PutMapping(value = "/account/i/{id}", consumes = "application/json")
-	public ResponseEntity<Account> updateById (@RequestBody Account account, @PathVariable Long id);
+	@GetMapping("/account/i/{id}")
+	ResponseEntity<Account> getAccountById(@PathVariable Long id);
 	
-	@DeleteMapping("/account/i/{id}")
-	public ResponseEntity<String> deleteById(@PathVariable Long id);
+	@GetMapping("/account/an/{accountNumber}")
+	ResponseEntity<Account> getAccountByNumber(@PathVariable Long accountNumber);
+	
+	@GetMapping("/account/nc/{customerNationalCode}")
+	ResponseEntity<Iterable<Account>> getAccountsByCustomerNationalCode(@PathVariable Long customerNationalCode);
 	
 	@PostMapping(value = "/account", consumes = "application/json")
-	public ResponseEntity<Account> saveAccount (@RequestBody Account account);
+	ResponseEntity<Account> saveAccount(@RequestBody Account account);
+	
+	@PutMapping(value = "/account", consumes = "application/json")
+	ResponseEntity<Account> updateAccount(@RequestBody Account account);
+	
+	@PutMapping(value = "/account/an/{accountNumber}", consumes = "application/json")
+	ResponseEntity<Account> updateByAccountNumber(@RequestBody Account account, @PathVariable Long accountNumber);
+	
+	@PutMapping(value = "/account/i/{id}", consumes = "application/json")
+	ResponseEntity<Account> updateById(@RequestBody Account account, @PathVariable Long id);
+	
+	@DeleteMapping("/account")
+	ResponseEntity<String> deleteAccount(@RequestBody Account account);
+	
+	@DeleteMapping("/account/an/{accountNumber}")
+	ResponseEntity<String> deleteByAccountNumber(@PathVariable Long accountNumber);
+	
+	@DeleteMapping("/account/i/{id}")
+	ResponseEntity<String> deleteById(@PathVariable Long id);
 }
