99a26b05346f626f544d10734eeb5d1157822385
sadeghifard <h.sadeghifard2@gmail.com>
2022-06-23 20:20:20 -0800

    Develop

--- moghilan-cloud-payment-microservice/src/main/java/com/sadeghifard/moghilan/controller/impl/ExceptionHandlerController.java
+++ moghilan-cloud-payment-microservice/src/main/java/com/sadeghifard/moghilan/controller/impl/ExceptionHandlerController.java
@@ -2,69 +2,98 @@ package com.sadeghifard.moghilan.controller.impl;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.ResourceClosedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import com.sadeghifard.moghilan.controller.IExceptionHandlerController;
 import com.sadeghifard.moghilan.exception.ErrorMessage;
-import com.sadeghifard.moghilan.exception.ResourceException;
+import com.sadeghifard.moghilan.exception.ResourceAlreadyReportedException;
+import com.sadeghifard.moghilan.exception.ResourceBadRequestException;
+import com.sadeghifard.moghilan.exception.ResourceNotAcceptableException;
+import com.sadeghifard.moghilan.exception.ResourceNotFoundException;
 
 @RestControllerAdvice
-public class ExceptionHandlerController implements IExceptionHandlerController {
+public class ExceptionHandlerController implements IExceptionHandlerController{
+	  
+	  @Override
+	  @ExceptionHandler(Exception.class)
+	  @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	  public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request) {
+	    ErrorMessage message = new ErrorMessage(
+		        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+		        LocalDateTime.now(),
+		        ex.getMessage(),
+		        request.getDescription(false));
+	    return message;
+	  }
+	  
+	  @Override
+	  @ExceptionHandler(ResourceNotFoundException.class)
+	  @ResponseStatus(value = HttpStatus.NOT_FOUND)
+	  public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+	    ErrorMessage message = new ErrorMessage(
+		        HttpStatus.NOT_FOUND.value(),
+		        LocalDateTime.now(),
+		        ex.getMessage(),
+		        request.getDescription(false));
+	    
+	    return message;
+	  }
 
 	@Override
-	@ExceptionHandler(Exception.class)
-	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-	public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request) {
-		ErrorMessage message = new ErrorMessage(
-										HttpStatus.INTERNAL_SERVER_ERROR.value(),
-										LocalDateTime.now(),
-										ex.getMessage(),
-										request.getDescription(false)
-										);
-		return message;
-	}
-
-	@Override
-	@ExceptionHandler(ResourceException.class)
-	@ResponseStatus(value = HttpStatus.NOT_FOUND)
-	public ErrorMessage resourceNotFoundException(ResourceException ex, WebRequest request) {
-		ErrorMessage message = new ErrorMessage(
-										HttpStatus.NOT_FOUND.value(),
-										LocalDateTime.now(),
-										ex.getMessage(),
-										request.getDescription(false)
-										);
-		return message;
-	}
-
-	@Override
-	@ExceptionHandler(ResourceException.class)
+	@ExceptionHandler(ResourceAlreadyReportedException.class)
 	@ResponseStatus(value = HttpStatus.ALREADY_REPORTED)
-	public ErrorMessage resourseAlreadyReportedException(ResourceException ex, WebRequest request) {
+	public ErrorMessage resourseAlreadyReportedException(ResourceAlreadyReportedException ex, WebRequest request) {
 		ErrorMessage message = new ErrorMessage(
-										HttpStatus.ALREADY_REPORTED.value(),
-										LocalDateTime.now(),
-										ex.getMessage(),
-										request.getDescription(false)
-										);
+				HttpStatus.ALREADY_REPORTED.value(),
+				LocalDateTime.now(),
+				ex.getMessage(),
+				request.getDescription(false)
+				);
 		return message;
 	}
 
 	@Override
-	@ExceptionHandler(ResourceException.class)
+	@ExceptionHandler(ResourceNotAcceptableException.class)
 	@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
-	public ErrorMessage resourseNotAcceptableException(ResourceException ex, WebRequest request) {
+	public ErrorMessage resourseNotAcceptableException(ResourceNotAcceptableException ex, WebRequest request) {
 		ErrorMessage message = new ErrorMessage(
-										HttpStatus.ALREADY_REPORTED.value(),
-										LocalDateTime.now(),
-										ex.getMessage(),
-										request.getDescription(false)
-										);
+		        HttpStatus.NOT_ACCEPTABLE.value(),
+		        LocalDateTime.now(),
+		        ex.getMessage(),
+		        request.getDescription(false));
+
+		return message;
+	}
+
+
+	@Override
+	@ExceptionHandler(ResourceBadRequestException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public ErrorMessage resourseBadRequestException(ResourceBadRequestException ex, WebRequest request) {
+		ErrorMessage message = new ErrorMessage(
+		        HttpStatus.BAD_REQUEST.value(),
+		        LocalDateTime.now(),
+		        ex.getMessage(),
+		        request.getDescription(false));
+
+		return message;
+	}
+
+	@Override
+	@ExceptionHandler(ResourceClosedException.class)
+	@ResponseStatus(value = HttpStatus.REQUEST_TIMEOUT)
+	public ErrorMessage resourseClosedException(ResourceClosedException ex, WebRequest request) {
+		ErrorMessage message = new ErrorMessage(
+		        HttpStatus.REQUEST_TIMEOUT.value(),
+		        LocalDateTime.now(),
+		        ex.getMessage(),
+		        request.getDescription(false));
+
 		return message;
 	}
 
